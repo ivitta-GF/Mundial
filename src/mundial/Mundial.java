9a83d13bf3933bf960fe69f2360e03519a8f63a7
iva <ivallen84@gmail.com>
2026-06-15 16:02:32 -0600

    Inicio de proyecto
    Se inicia el proyecto mundial

---
+++ src/mundial/Mundial.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mundial;
+
+/**
+ *
+ * @author PC
+ */
+public class Mundial {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    
+}
