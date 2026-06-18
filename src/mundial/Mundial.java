d53ff303ca7dfd70825972df4df8a75617966745
iva <ivallen84@gmail.com>
2026-06-18 14:29:10 -0600

    Se creo clase persona
    Se creo persona y se hizo set y get

--- src/mundial/Mundial.java
+++ src/mundial/Mundial.java
@@ -9,6 +9,36 @@ package mundial;
  * @author PC
  */
 public class Mundial {
+    
+    private String nombreM;
+    private double year;
+    private String país;
+
+    public String getNombreM() {
+        return nombreM;
+    }
+
+    public double getYear() {
+        return year;
+    }
+
+    public String getPaís() {
+        return país;
+    }
+
+    public void setYear(double year) {
+        this.year = year;
+    }
+
+    public void setPaís(String país) {
+        this.país = país;
+    }
+    
+    
+    
+    
+    
+    
 
     /**
      * @param args the command line arguments
---
+++ src/personas/Personas.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personas;
+
+/**
+ *
+ * @author PC
+ */
+public class Personas {
+    private String nombre;
+    private String fecha;
+    private String nacionalidad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
