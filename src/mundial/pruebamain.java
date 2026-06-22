5c5de72a983b1b5f70b12f255a961997876b2d5c
maripacita <mariichavarriajerez29@gmail.com>
2026-06-22 16:39:03 -0600

    Uso Main
    Se uso el metodo main

--- src/mundial/pruebamain.java
+++ src/mundial/pruebamain.java
@@ -13,8 +13,6 @@ public class pruebamain {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+   
     
 }
